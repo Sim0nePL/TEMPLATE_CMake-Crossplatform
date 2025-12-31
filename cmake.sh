80989fa3deedd53cc18c8884bafdd752d34228f6
Sim0nePL <sim0nepl.krzyk.public@proton.me>
2025-12-31 22:10:01 +0100

    Add toolchains

--- cmake.sh
+++ cmake.sh
@@ -4,6 +4,15 @@ echo "-- [ Generating Buildfiles For Linux ] --"
 cmake \
 	-S . \
 	-B build/linux \
-	-G Ninja
+	-G Ninja \
+	--toolchain toolchains/linux-toolchain.cmake 
+echo ""
+
+echo "-- [ Generating Buildfiles For Windows ] --"
+cmake \
+	-S . \
+	-B build/windows \
+	-G Ninja \
+	--toolchain toolchains/windows-toolchain.cmake 
 echo ""
 
